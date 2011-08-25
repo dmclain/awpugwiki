89c100983b2c27942dd43d7ece33473bdf36bb49
Dave McLain <github@davemclain.com>
2011-08-25 15:38:28 -0500

    New page

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, CreateView
 from .models import Page
 
 urlpatterns = patterns('',
     url(r'^$', ListView.as_view(model=Page), name='page_list'),
+    url(r'^new$', CreateView.as_view(model=Page), name='page_new'),
     url(r'^(?P<slug>\w+)$', DetailView.as_view(model=Page), name='page_view'),
     # url(r'^awpugwiki/', include('awpugwiki.foo.urls')),
 )
